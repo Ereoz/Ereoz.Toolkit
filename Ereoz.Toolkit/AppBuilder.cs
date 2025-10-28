4610cafea7c25c0313bacd65d3e43e082e361a2f
Denis Plotnikov <denereoz@gmail.com>
2025-10-28 11:33:16 +0700

    Refactor auto registers

--- Ereoz.Toolkit/AppBuilder.cs
+++ Ereoz.Toolkit/AppBuilder.cs
@@ -3,8 +3,11 @@ using Ereoz.DI;
 using Ereoz.MVVM;
 using Ereoz.WindowManagement;
 using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Reflection;
 using System.Windows;
+using System.Windows.Documents;
 
 namespace Ereoz.Toolkit
 {
@@ -41,11 +44,35 @@ namespace Ereoz.Toolkit
 
         public T CreateMainWindow<T>(WindowLocation appSettings = null) where T : Window
         {
+            List<Type> allTypes = null;
+
+            if (_isAutoRegisterContracts || _isAutoRegisterContracts)
+            {
+                allTypes = AppDomain.CurrentDomain
+                    .GetAssemblies()
+                    .SelectMany(assembly => assembly.GetTypes())
+                    .Where(type => !type.FullName.StartsWith("System")
+                                && !type.FullName.StartsWith("Microsoft")
+                                && !type.FullName.StartsWith("Windows")
+                                && !type.FullName.StartsWith("Interop")
+                                && !type.FullName.StartsWith("Internal")
+                                && !type.FullName.StartsWith("StartupHook")
+                                && !type.FullName.StartsWith("FxResources")
+                                && !type.FullName.StartsWith("ThisAssembly")
+                                && !type.FullName.StartsWith("FXAssembly")
+                                && !type.FullName.StartsWith("AssemblyRef")
+                                && !type.FullName.StartsWith("MatchState")
+                                && !type.FullName.StartsWith("EmptyArray")
+                                && !type.FullName.StartsWith("<")
+                                && !type.FullName.StartsWith("_"))
+                    .ToList();
+            }
+
             if(_isAutoRegisterContracts)
-                ((ServiceContainer)ServiceContainer).AutoRegisterAllContracts();
+                ((ServiceContainer)ServiceContainer).AutoRegisterAllContracts(allTypes);
 
             if (_isAutoRegisterViewWithViewModels)
-                _navigationManager.AutoRegisterAllViewsWithViewModels();
+                _navigationManager.AutoRegisterAllViewsWithViewModels(allTypes);
 
             return _navigationManager.CreateMainWindow<T>(appSettings);
         }
